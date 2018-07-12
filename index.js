d88636c0901aea172394244634b8357968396889
zernax <40726857+zernax@users.noreply.github.com>
2018-07-12 03:20:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const Discord = require("discord.js");
+const snek = require("snekfetch");
 const client = new Discord.Client();
 const Cleverbot = require("cleverbot-node");
 const clbot = new Cleverbot;
@@ -24,15 +25,11 @@ client.on('message', (message) => {
  if(message.mentions.members.first().id == "466476840253521920"){
     args.shift()
  args.join(" ")
+ var message = args.join(" ")
+ var url = `https://api.dialogflow.com/v1/query?v=20170712&query=${message}!&lang=fr&sessionId=f93516c0-58f7-1286-f79b-afd71932b11b&timezone=America/Toronto`
+snek.get(url,{headers:{"Authorization":"Bearer d7ea978ee0f54a64b25692ef39a3de6e"}}).then(function(response){message.reply(response.body.result.fulfillment.speech)})
  
-   clbot.write(args.join(" "), (response) => {
-      message.channel.startTyping();
-      setTimeout(() => {
-        message.channel.send(response.output).catch(console.error);
-        message.channel.stopTyping();
-      }, Math.random() * (1 - 3) + 1 * 1000);
-     
-    });
+  
  }
   }
  
