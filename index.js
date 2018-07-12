9a4315352c16ed89a9ffb0c8f7f991febeb46197
zernax <40726857+zernax@users.noreply.github.com>
2018-07-12 02:22:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const Cleverbot = require("cleverbot-node");
 const clbot = new Cleverbot;
+var cleverbotz = {};
+
 clbot.configure({botapi: process.env.BOT_CLEVER});
 client.on("ready", () => {
  
@@ -18,12 +20,16 @@ client.on('message', (message) => {
   
   if (message.author.equals(client.user)) return;
   if (message.mentions.members.size !== 0){
+    if(message.content == "!instance"){
+ cleverbot = new Cleverbot;
+        cleverbotz[message.sender.id] = cleverbot;
+ }
      
  if(message.mentions.members.first().id == "466476840253521920"){
     args.shift()
  args.join(" ")
-   
-   clbot.write(args.join(" "), (response) => {
+   if (cleverbotz[message.sender.id]) {
+   cleverbotz[message.sender.id].write(args.join(" "), (response) => {
       message.channel.startTyping();
       setTimeout(() => {
         message.channel.send(response.output).catch(console.error);
@@ -33,6 +39,8 @@ client.on('message', (message) => {
     });
  }
   }
+ }
+ 
   });
 
 client.login(process.env.BOT_TOKEN);
